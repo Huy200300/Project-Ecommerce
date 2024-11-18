16e64337bc0a7e66898244a52a7324d56f36cf2f
Huy200300 <taihuy200300@gmail.com>
2024-11-18 19:28:49 +0700

    update  user

--- Back-End-User/index.js
+++ Back-End-User/index.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT || 8080;
 connectDB();
 
 app.use(
